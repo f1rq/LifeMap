73bfcb10285ea867b6989681611a049e899df6c4
f1rq <f.czudaj@tutanota.com>
2025-10-27 13:34:33 +0100

    Added dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.foundation)
+    implementation(libs.androidx.datastore.core)
+    implementation(libs.androidx.datastore.preferences.rxjava3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
